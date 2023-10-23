eb9c36d3198ea605994e7ada4e2e2779f146f76a
rishavpandey02 <rishavpandey@outlook.com.au>
2023-10-23 20:32:27 +1100

    Deleted

--- api/index.js
+++ api/index.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.js'
+import authRouter from './routes/auth.js'
+dotenv.config();
 
 mongoose.connect('mongodb+srv://rishavpandey:Brssp123@mern-carrental.wclvith.mongodb.net/?retryWrites=true&w=majority')
 
@@ -7,5 +11,8 @@ const app = express();
 
 app.listen(3000, () => {
 console.log('Server is running on port 3000')
-}
-);
+});
+
+app.use("/api/user", userRouter)
+app.use('/api/auth', authRouter)
+
