23aafa4ace152947ccb456cf833198faddfe7bd3
rishavpandey02 <rishavpandey@outlook.com.au>
2023-10-23 19:45:20 +1100

    first commit, header and react file

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import { Header } from './components/Header'
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login />}/>
+      <Route path="/signUp" element={<SignUp />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/profile" element={<Profile />}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { FaSearch } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+export const Header = () => {
+  return (
+    <header className='bg-slate-800'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link to="/">
+        <div className=''>
+        <h1 className="font-bold text-sm sm:text-sm flex flex-wrap">
+            <span className='text-white text-3xl'>Luxe</span>
+            <span className='text-red-600 text-3xl'>Wheels</span>
+        </h1>
+        </div>
+        </Link>
+        <form className='flex items-center rounded-lg p-2 bg-white'>
+            <input type="text" placeholder='Search...' className='bg-transparent p-1 focus:outline-none w-24 sm:w-64' />
+            <FaSearch className='text-slate-600'/>
+        </form>
+        <ul className='flex gap-4 text-white'>
+            <Link to="/">
+            <li className='hidden sm:inline hover:underline'>Home</li>
+            </Link>
+            <Link to="/about">
+            <li className='hidden sm:inline hover:underline'>About Us</li>
+            </Link>
+            <Link to="/Login">
+            <li className='hover:underline'>Sign in</li>
+            </Link>
+        </ul>
+        </div>
+        
+    </header>
+  )
+}
