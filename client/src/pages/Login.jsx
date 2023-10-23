6da9f9286bfc0748e631d7e4070b9cc794ae20f4
rishavpandey02 <rishavpandey@outlook.com.au>
2023-10-23 21:07:51 +1100

    finished login page design

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Login() {
   return (
-    <div>
-        <h2>Login</h2>
-        
+    <div className='p-3 max-w-lg mx-auto'>
+    <h1 className='text-3xl text-center font-sembold my-6 text-white'>Login</h1>
+
+    <form className='flex flex-col gap-4'>
+        <input type="text" placeholder='username' className='border-blue-900 border-2 p-3 rounded-lg bg-black text-white' id='username'/>
+        <input type="email" placeholder='email' className='border-blue-900 border-2 p-3 rounded-lg bg-black text-white' id='email'/>
+        <input type="password" placeholder='password' className='border-blue-900 border-2 p-3 rounded-lg bg-black text-white' id='password'/>
+
+        <button className='bg-blue-700 text-white py-2 rounded-lg hover:opacity-80 disabled: opacity-80'>SIGN UP</button>
+    </form>
+    <div className='flex gap-2 mt-4'>
+        <p>Don't have an account?</p>
+        <Link to={"/signup"}>
+            <span className='text-red-500 hover:underline'>Sign Up</span>
+        </Link>
     </div>
+</div>
   )
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -8,15 +8,15 @@ export default function SignUp() {
         <h1 className='text-3xl text-center font-sembold my-6 text-white'>SignUp</h1>
 
         <form className='flex flex-col gap-4'>
-            <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username'/>
-            <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email'/>
-            <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password'/>
+            <input type="text" placeholder='username' className='border-blue-900 border-2 p-3 rounded-lg bg-black text-white' id='username'/>
+            <input type="email" placeholder='email' className='border-blue-900 border-2 p-3 rounded-lg bg-black text-white' id='email'/>
+            <input type="password" placeholder='password' className='border-blue-900 border-2 p-3 rounded-lg bg-black text-white' id='password'/>
 
             <button className='bg-blue-700 text-white py-2 rounded-lg hover:opacity-80 disabled: opacity-80'>SIGN UP</button>
         </form>
         <div className='flex gap-2 mt-4'>
             <p>Already have an account?</p>
-            <Link to={"/sign-in"}>
+            <Link to={"/login"}>
                 <span className='text-red-500 hover:underline'>Sign In</span>
             </Link>
         </div>
