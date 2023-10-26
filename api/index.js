02a0d241ab7d829490945992713e8ccb8b449f9a
rishavpandey02 <rishavpandey@outlook.com.au>
2023-10-26 17:46:59 +1100

    start of backend and bit of homepage done

--- api/index.js
+++ api/index.js
@@ -1,18 +1,19 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './routes/user.js'
-import authRouter from './routes/auth.js'
 dotenv.config();
 
-mongoose.connect('mongodb+srv://rishavpandey:Brssp123@mern-carrental.wclvith.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log(err);
+});
+
 
 const app = express();
 
 app.listen(3000, () => {
 console.log('Server is running on port 3000')
 });
-
-app.use("/api/user", userRouter)
-app.use('/api/auth', authRouter)
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <div classname=''>
+            <h1>Rent the Best <span>Quality Car's</span> 
+            <br/>
+            With Us </h1>
+            <div>
+                <p>LuxeWheels will ensure that you have a pleasurable experience.
+                <br/>
+                We have a widen range of luxury vehicle for you to choose from.</p>
+            </div>
+        </div>
+    </div>
   )
 }
