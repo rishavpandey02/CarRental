a34cad049c948e91dc2cae9ec494f49038503358
rishavpandey02 <rishavpandey@outlook.com.au>
2023-10-31 07:43:34 +1100

    fixing server

--- api/index.js
+++ api/index.js
@@ -21,9 +21,7 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.listen(3001, () => {
-console.log('Server is running on port 3001')
-});
+
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
@@ -39,3 +37,6 @@ app.use((err, req, res, next) => {
     });
 });
 
+app.listen(3001, () => {
+    console.log('Server is running on port 3001')
+    });
