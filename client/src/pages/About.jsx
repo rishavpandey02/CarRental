a8abad6d72277b8a25b95009d565939b225d8080
rishavpandey02 <rishavpandey@outlook.com.au>
2023-10-31 17:34:46 +1100

    Finished about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,27 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-white'>About LuxeWheels</h1>
+      <p className='mb-4 text-white'>Welcome to LuxeWheels, where luxury meets convenience in the world of car rentals! At LuxeWheels, we understand that a car is more than just a means of transport - it's an extension of your lifestyle, a statement of your taste, and often, a necessity tailored to your specific needs. That's why we've created a platform that seamlessly connects discerning car owners with individuals seeking to rent a premium vehicle for business, pleasure, or a unique driving experience.</p>
+      <h1 className='text-3xl font-bold mb-4 text-white'>Our Vision</h1>
+      <p className='mb-4 text-white'>
+      LuxeWheels was born out of a vision to redefine the car rental landscape. We noticed a gap in the market for high-end, well-maintained, and luxury vehicles that could be rented directly from trustworthy and discerning car owners. We wanted to create a community where luxury car enthusiasts could both share and experience the joy of driving some of the world’s most sought-after vehicles.</p>
+
+      <h1 className='text-3xl font-bold mb-4 text-white'>What we Offer</h1>
+      <p className='mb-4 text-white'>
+        <span className='text-gray-400 mr-1'>1.</span>
+        <span className='font-bold mr-1'>For Car Owners:</span>
+        If you're a luxury car owner, LuxeWheels offers you an easy and secure way to monetize your vehicle when it's not in use. We understand the value and emotional attachment you have to your car. That’s why we provide a platform that ensures your prized possession is rented out responsibly and to individuals who appreciate and understand the value of a luxury vehicle.
+      </p>
+      <p className='mb-4 text-white'>
+        <span className='text-gray-400 mr-1'>2.</span>
+        <span className='mr-1 font-bold'>For Renters:</span>
+        For those looking to rent, LuxeWheels provides an exceptional range of high-end vehicles to choose from. Whether you need a classy sedan for a business trip, a sporty convertible for a weekend getaway, or a luxury SUV for a family vacation, our platform ensures you have access to the best vehicles in town, directly from owners who care about their cars.
+      </p>
+      <h1 className='text-3xl font-bold mb-4 text-white'>Our Commitment to Quality and Safety</h1>
+      <p className='mb-4 text-white'>Safety and satisfaction are paramount at LuxeWheels. We meticulously vet all vehicles and renters to ensure that every transaction is secure and meets our high standards. Our platform features a straightforward booking process, transparent pricing, and comprehensive insurance coverage to ensure peace of mind for both car owners and renters.</p>
+    
+    </div>
   )
 }
